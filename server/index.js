79416af5b5d976e5de4030aa419e51eee25207b0
nitorz <kennethfrancis800@gmail.com>
2025-11-20 17:42:59 +0300

    Add and update client and server .gitignore files

--- server/index.js
+++ server/index.js
@@ -1,3 +1,13 @@
-import analyticsRoutes from "./routes/analytics.js";
+import path from "path";
+import express from "express";
 
-app.use("/api/analytics", analyticsRoutes);
+const app = express();
+
+// Serve frontend build
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+});
+
+export default app;
